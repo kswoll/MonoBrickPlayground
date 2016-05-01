d15952aea778f36b66c770641c075f039c177987
Kirk Woll <kirk.woll@gmail.com>
2016-05-01 15:30:00 -0700

    Initial Commit

---
+++ MonoBrickUtils/Ev3Console.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MonoBrickFirmware.Display;
+
+namespace MonoBrickUtils
+{
+    public static class Ev3Console
+    {
+        private static readonly Font font = Font.SmallFont;
+        private static readonly int lineHeight = (int)font.maxHeight;
+        private static readonly string[] buffer = new string[Lcd.Height / lineHeight];
+        private static int line;
+
+        public static void WriteLine(object o)
+        {
+            WriteLine(o == null ? "" : o.ToString());
+        }
+
+        public static void WriteLine(string text) 
+        {
+            if (line < buffer.Length)
+            {
+                buffer[line++] = text;
+            }
+            else
+            {
+                for (var i = 0; i < buffer.Length - 1; i++)
+                {
+                    buffer[i] = buffer[i + 1];
+                }
+                buffer[buffer.Length - 1] = text;
+            }
+
+            Render();
+        }
+
+        private static void Render() 
+        {
+            Lcd.Clear();
+            for (var i = 0; i <= Math.Min(buffer.Length, line - 1); i++)
+            {
+                Lcd.WriteText(font, new Point(0, lineHeight * i), buffer[i], true);
+            }
+            Lcd.Update();
+        }
+    }
+}
+
---
+++ Robot1/Robot1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MonoBrickFirmware.Display.Dialogs;
+using MonoBrickFirmware.Movement;
+using System.Threading;
+using MonoBrickFirmware.Display;
+using MonoBrickUtils;
+using MonoBrickFirmware.Sensors;
+
+namespace Robot1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                var sensor = new EV3IRSensor(SensorPort.In4);
+                Ev3Console.WriteLine($"IR: {sensor.ReadDistance()}");
+            }
+            Thread.Sleep(10000);
+        }
+    }
+}
