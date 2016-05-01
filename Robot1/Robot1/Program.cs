6e1c33b679e064ca6f80346e51b5d19c489c775f
Kirk Woll <kirk.woll@gmail.com>
2016-05-01 16:36:38 -0700

    Add more samples for different sensor types

--- Robot1/Robot1/Program.cs
+++ Robot1/Robot1/Program.cs
@@ -12,12 +12,78 @@ namespace Robot1
     {
         public static void Main(string[] args)
         {
+//            ReadFromEV3IRSensor();
+//            ReadFromHiTechnicCompassSensor();
+//            ReadFromHiTechnicTiltSensor();
+//            ReadFromNXTSoundSensor();
+            ReadFromEV3TouchSensor();
+            Thread.Sleep(10000);
+        }
+
+        static void ReadFromEV3IRSensor()
+        {
             for (var i = 0; i < 1000; i++)
             {
                 var sensor = new EV3IRSensor(SensorPort.In4);
-                Ev3Console.WriteLine($"IR: {sensor.ReadDistance()}");
+                var distance = sensor.ReadDistance();
+                Ev3Console.WriteLine($"IR: {distance}");
+                Thread.Sleep(10);
             }
-            Thread.Sleep(10000);
+        }
+
+        static void ReadFromHiTechnicCompassSensor()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                var sensor = new HiTecCompassSensor(SensorPort.In4);
+                var heading = sensor.ReadDirection();
+                Ev3Console.WriteLine($"IR: {heading}");
+                Thread.Sleep(10);
+            }
+        }
+
+        static void ReadFromHiTechnicTiltSensor()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                var sensor = new HiTecTiltSensor(SensorPort.In4);
+                var position = sensor.ReadPosition();
+                Ev3Console.WriteLine($"IR: {position.X}/{position.Y}/{position.Z}");
+                Thread.Sleep(10);
+            }
+        }
+
+        static void ReadFromNXTSoundSensor()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                var sensor = new NXTSoundSensor(SensorPort.In4);
+                var value = sensor.ReadAsString();
+                Ev3Console.WriteLine($"IR: {value}");
+                Thread.Sleep(10);
+            }
+        }
+
+        static void ReadFromNXTTouchSensor()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                var sensor = new NXTTouchSensor(SensorPort.In4);
+                var value = sensor.ReadAsString();
+                Ev3Console.WriteLine($"IR: {value}");
+                Thread.Sleep(10);
+            }
+        }
+
+        static void ReadFromEV3TouchSensor()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                var sensor = new EV3TouchSensor(SensorPort.In4);
+                var value = sensor.ReadAsString();
+                Ev3Console.WriteLine($"IR: {value}");
+                Thread.Sleep(10);
+            }
         }
     }
 }
